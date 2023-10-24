d13a9ab8b438ddb01caaf50b998741fe02929bdd
Michael DiBenigno <michael@flow.gl>
2023-10-24 11:34:41 -0700

    basic files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add your D3.js code here
+// Example for line chart:
+ var svg = d3.select("#line-chart").append("svg");
+
+// Example for bar chart:
+ var svg = d3.select("#bar-chart").append("svg");
+
+// Example for map:
+ var svg = d3.select("#map").append("svg");
